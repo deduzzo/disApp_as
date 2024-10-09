95b9c74031de89d500830c5d524a45083f5360f3
Roberto De Domenico <roberto.dedomenico@asp.messina.it>
2024-10-09 14:01:56 +0000

    fix tabs

--- pages/Home/jsobjects/functions/functions.js
+++ pages/Home/jsobjects/functions/functions.js
@@ -1,15 +1,26 @@
 export default {
-	aggiornaQueryTabs() {
-		getConteggiTotaliDetermina.run();
-		getAllPagamentiDisDet.run();
-		getAllIstanzeDistretto.run();
-	},
-	aggiornaTabs() {
+	async aggiornaTabs() {
 		storeValue('selectedTab',tabs.selectedTab);
 		switch(tabs.selectedTab) {
-				case "Pagamenti":
-				this.aggiornaQueryTabs();
+			case "Pagamenti":
+				await getConteggiTotaliDetermina.run()
+				await getAllPagamentiDisDet.run();
+				break;
+			case "Elenco":
+				await getAllIstanzeDistretto.run();
+				break;
+			case "Scheda":
+				disabile_select.setSelectedOption(appsmith.store.selectedRowId ?? null);
 				break;
 		}
+	},
+	async initLoad() {
+		storeValue('selectedTab',"Elenco");
+		await getAllDistretti.run();
+		await getAllIstanzeDistretto.run();
+		await getAllDeterminePagamenti.run();
+	},
+	cangeTabs() {
+		this.aggiornaTabs();
 	}
 }
