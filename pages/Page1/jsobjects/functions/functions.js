41fdafb5634dcb2988880f17efd633201ff414c9
Roberto De Domenico <roberto.dedomenico@asp.messina.it>
2024-10-09 11:03:04 +0000

    fix dati

--- pages/Page1/jsobjects/functions/functions.js
+++ pages/Page1/jsobjects/functions/functions.js
@@ -1,18 +1,6 @@
 export default {
-	async aggiornaTable() {
-		try {
-			// Attendi il caricamento dei dati da 'getConteggiTotaliDetermina'
-			let dati = await getConteggiTotaliDetermina.data;
-		
-
-			// Esegui 'getAllPagamentiDisDet' e attendi il completamento
-			await getAllPagamentiDisDet.run();
-
-			// Se arriva qui, significa che 'getAllPagamentiDisDet' ha avuto successo
-			showAlert('Dati caricati con successo', "success");
-		} catch (error) {
-			// Se c'è un errore durante l'esecuzione della query, lo gestiamo qui
-			showAlert('Errore durante la richiesta dei dati', 'error');
-		}
-	}
+	aggiornaTable() {
+		getConteggiTotaliDetermina.run()
+		getAllPagamentiDisDet.run();
+	},
 }
